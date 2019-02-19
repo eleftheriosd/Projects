da9bfdb865623d945f83db3fb4b681e5eea6ee83
Lefteris <eleftherios.davros@gmail.com>
2019-02-19 07:58:26 +0200

    React Infinite Scroll with Backend

---
+++ React_infinite_scroll/config/server.js
@@ -0,0 +1,5 @@
+module.exports={
+    APPLICATION_ID: process.env.APPLICATION_ID || 'c225c2d30d09536bafca3147de1e4a68513771558b4435909d2b4507c15dd570',
+    SECRET: process.env.SECRET || '3a1ec016c6afc3438c3703a4672f8325183f0db8bd08b51d0175a97a45ab9374',
+    CALLBACK_URL : process.env.CALLBACK_URL || 'http://localhost:3000'
+}
